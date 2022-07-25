e687b0741c435df9d8fad41a218d3685c9970e89
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-26 00:59:13 +0300

    adding the books to the main page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import logo from '../asset/resource/bookslogo.png';
+import { getBooks } from './consumeAPI.js';
 
 const logoImage = document.querySelector('.logo-img');
 logoImage.src = logo;
+
+getBooks();
