2e356a07b36736f0404a4baa0e011d8140e4c12c
jaferIdrees <jaferll@gmail.com>
2022-07-27 19:14:04 +0300

    Send comment data to Involvment API

--- src/commentsHandler.js
+++ src/commentsHandler.js
@@ -1,32 +1,22 @@
 import Comment from './Comment.js';
 
 const commentsUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/txIV0x61cJerTV943Ps8/comments';
-const test = JSON.stringify({
-  'item_id': "item1",
-  'username': "Jane",
-  'comment': "Hello"
-});
+
 const postComment = async (comment) => {
-  const commentA = JSON.stringify({
-    item_id: comment.item_id,
-    username: comment.username,
-    comment: comment.comment,
-  });
-  console.log(commentA);
   await fetch(commentsUrl, {
     method: 'POST',
     headers: {
       'Content-type': 'application/json; charset=utf-8',
-      Accept: 'application/json',
     },
-    body: `${commentA}`,
+    body: JSON.stringify(comment),
   }).then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.log(err));
+    .then((json) => json)
+    .catch((err) => err);
 };
 
 const comment = (e) => {
-  const title = document.querySelector('.bookCard').querySelector('.title').innerText;
+  e.preventDefault();
+  const title = String(document.querySelector('.bookCard').querySelector('.title').innerText);
   const user = e.target.parentElement.querySelector('#userName').value;
   const commentText = e.target.parentElement.querySelector('#commentText').value;
   const comment = new Comment(title, user, commentText);
