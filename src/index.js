bf698882251305f1e69ac74f7bc86c058393129f
Jafar Yousef <jaferll@gmail.com>
2022-07-28 09:28:28 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { getBooks } from './consumeAPI.js';
 import { comment } from './commentsHandler.js';
 import bookCounter from './booksCounter.js';
 
-
 const logoImage = document.querySelector('.logo-img');
 logoImage.src = logo;
 document.querySelector('.commentForm').onsubmit = comment;
